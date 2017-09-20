a78f59b0fedd0e163d44dd130512bbf56821fa0c
João V. B. Silva <joaovitorbarbosa@uft.edu.br>
2017-09-20 16:21:23 -0300

    Página Home aprimorada para demonstração de Cards, e testes com inputs de tempo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,24 +8,43 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   blocoIndice: string = "Bloco 3";
-  n: number = 0;
+  dia: any;
+  horario: any;
+  items =  [{sala: 'lab 11', ar: 'ligado', ds: 'ligado'},
+            {sala: 'sala 39', ar: 'desligado', ds: 'desligado'},
+            {sala: 'lab 5', ar: 'ligado', ds: 'desligado'}];
 
-  constructor(public navCtrl: NavController) {}
+  constructor(
+    public navCtrl: NavController,
+    public actionSheetCtrl: ActionSheetController) {
+
+  }
 
   ionViewDidLoad() {}
 
-  nN():number {
-    this.n = this.n + 1;
-    return this.n;
-  }
+  mudarBloco() {
+   let actionSheet = this.actionSheetCtrl.create({
+     title: 'Ir para:',
+     buttons: [
+       {
+         text: 'Bloco 3',
+         handler: () => {
+           this.blocoIndice = "Bloco 3";
+           console.log('Bloco trocado: ' + this.blocoIndice);
+         }
+       },
+       {
+         text: 'Bloco 4',
+         handler: () => {
+           this.blocoIndice = "Bloco 4";
+           console.log('Bloco trocado: ' + this.blocoIndice);
+         }
+       }
+     ]
+   });
 
-  mostrar(){
-    var div = document.getElementById('0');          // obtem a DIV pelo ID
-    if (div.style.display == 'none')                      // se conteúdo está escondido
-        div.style.display = 'block';                      // mostra o conteúdo
-    else                                                  // se conteúdo está a mostra
-        div.style.display = 'none';                       // esconde o conteúdo
-  }
+   actionSheet.present();
+ }
 
 }
 
